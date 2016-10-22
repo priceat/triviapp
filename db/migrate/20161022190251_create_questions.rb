ab81d901f01d8a8238d65db4de9a21ea96e109f3
Tate Price <priceat@gmail.com>
2016-10-22 15:20:39 -0500

    Create Questions

---
+++ db/migrate/20161022190251_create_questions.rb
@@ -0,0 +1,12 @@
+class CreateQuestions < ActiveRecord::Migration
+  def change
+    create_table :questions do |t|
+
+      t.string :type
+      t.text :question
+      t.text :answer
+
+      t.timestamps
+    end
+  end
+end
